34d0c5c009f18093b4980f9d1326148dc2ab93c5
ThomasBettencourt <thomasbetten.cs@gmail.com>
2020-01-24 19:19:24 -0600

    prototypes done

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -77,8 +77,9 @@ const graduates = [
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array. This will be an array of strings.
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
-const universities = [];
-console.log(universities);
+
+const universities = graduates.map(x => x.university);
+console.log(universities.sort());
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
 
@@ -87,9 +88,11 @@ The resulting contact information strings should have a space between the first 
 
 Log the result of your new array. */
 const contactInfo = [];
+
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
+
 const unisWithUni = [];
 console.log(unisWithUni);
 
